8a7ea5401f882665b0f4e80c85834d6de70af6e8
Zachary Davy <zacharyjdavy@gmail.com>
2014-03-10 21:37:31 -0500

    first commit

---
+++ src/umwelt/test/_Sample.java
@@ -0,0 +1,12 @@
+package umwelt.test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class _Sample
+{
+  @Test
+  public void NewTest()
+  {
+    assertEquals(0, 0);
+  }
+}
