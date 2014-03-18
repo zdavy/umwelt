5299baeed4693937da4144927cca5abe86b1156e
Zachary Davy <zacharyjdavy@gmail.com>
2014-03-18 16:10:57 -0500

    get handler proccesses the request

--- src/umwelt/mocks/Communication/_UmweltRequest.java
+++ src/umwelt/mocks/Communication/_UmweltRequest.java
@@ -3,5 +3,16 @@ package umwelt.mocks.Communication;
 import umwelt.server.Communication.iRequest;
 
 public class _UmweltRequest implements iRequest {
+  public boolean proccessed = false;
+  public _UmweltRequest() {
+  }
 
+  public String method() {
+    return "get";
+  }
+
+  public String uri() {
+    proccessed = true;
+    return "/test";
+  }
 }
--- src/umwelt/test/Handlers/GetHandlerTest.java
+++ src/umwelt/test/Handlers/GetHandlerTest.java
@@ -2,10 +2,18 @@
 package umwelt.test.Handlers;
 
 import org.junit.Test;
+
+import umwelt.mocks.Communication._UmweltRequest;
+import umwelt.mocks.Communication._UmweltResponse;
+import umwelt.server.Handlers.GetHandler;
+
 import static org.junit.Assert.*;
 
 public class GetHandlerTest {
-  @Test public void test() {
-    assertTrue(true);
+  @Test public void responseIsProccessedWithRequestData() {
+    GetHandler.addRoute("/test", new _UmweltResponse());
+    _UmweltRequest request = new _UmweltRequest();
+    GetHandler.handle(request);
+    assertTrue(request.proccessed);
   }
 }
